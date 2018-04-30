412fa1c601e4d1dfb500801f304cbdfff3b51d39
eringee <erin.marie.gee@gmail.com>
2018-04-30 16:23:35 +0200

    Added BETA respiration library

---
+++ Respiration.cpp
@@ -0,0 +1,122 @@
+/*
+ * Respiration.cpp
+ *
+ * This class defines an object that can be used to gather information about
+ * respiration. There are many ways to gather respiration data, such as through thermistor,
+ * thermopile, conductive rubber cord, or piezoelectric signals.
+ *
+ * This file is part of the BioData project
+ * (c) 2018 Erin Gee
+ *
+ * Contributing authors:
+ * (c) 2018 Erin Gee
+ * (c) 2018 Sofian Audry
+ * (c) 2017 Thomas Ouellet Fredericks
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "Respiration.h"
+
+Respiration::Respiration(uint8_t pin, unsigned long rate) :
+  _pin(pin),
+// Note: might want to make this as wide as possible, as irrelevant low-frequency noise
+//from the respiration sensor is unlikely
+  respThresh(0.5, 0.6),          // if signal does not fall below (low, high) bounds than signal is ignored
+  respSensorAmplitudeLop(0.001),
+  respSensorBpmLop(0.001)
+{
+  setSampleRate(rate);
+  reset();
+}
+
+void Respiration::reset() {
+  respSensorAmplitudeLop = Lop(0.001);
+  respSensorBpmLop = Lop(0.001);
+  respSensorAmplitudeLopValueMinMax = MinMax();
+  respSensorBpmLopValueMinMax = MinMax();
+  respSensorReading = respSensorFiltered = respSensorAmplitude = 0;
+  bpmChronoStart = millis();   // Note that in this case, BPM refers to "breaths per minute"  ;)
+  bpm = 20;
+  breath = false;
+
+  prevSampleMicros = micros();
+
+  // Perform one update.
+  sample();
+}
+
+void Respiration::setSampleRate(unsigned long rate) {
+  sampleRate = rate;
+  microsBetweenSamples = 1000000UL / sampleRate;
+}
+
+void Respiration::update() {
+  unsigned long t = micros();
+  if (t - prevSampleMicros >= microsBetweenSamples) {
+    // Perform updates.
+    sample();
+    prevSampleMicros = t;
+  }
+}
+
+float Respiration::getNormalized() const {
+  return respSensorFiltered;
+}
+
+float Respiration::amplitudeChange() const {
+  return respSensorAmplitudeLopValueMinMaxValue;
+}
+
+float Respiration::bpmChange() const {
+  return respSensorBpmLopValueMinMaxValue;
+}
+
+bool Respiration::breathDetected() const {
+  return breath;
+}
+
+float Respiration::getBPM() const {
+  return bpm;
+}
+
+int Respiration::getRaw() const {
+  return respSensorReading;
+}
+
+void Respiration::sample() {
+  // Read analog value if needed.
+  respSensorReading = analogRead(_pin);
+
+  respSensorFiltered = respMinMax.filter(respSensorReading);
+  respSensorAmplitude = respMinMax.getMax() - respMinMax.getMin();
+  respMinMax.adapt(0.01); // APPLY A LOW PASS ADAPTION FILTER TO THE MIN AND MAX
+
+  respSensorAmplitudeLopValue = respSensorAmplitudeLop.filter(respSensorAmplitude);
+  respSensorBpmLopValue =  respSensorBpmLop.filter(bpm);
+
+  respSensorAmplitudeLopValueMinMaxValue = respSensorAmplitudeLopValueMinMax.filter(respSensorAmplitudeLopValue);
+  respSensorAmplitudeLopValueMinMax.adapt(0.001);
+  respSensorBpmLopValueMinMaxValue = respSensorBpmLopValueMinMax.filter(respSensorBpmLopValue);
+  respSensorBpmLopValueMinMax.adapt(0.001);
+
+  breath = respThresh.detect(respSensorFiltered);
+
+  if ( breath ) {
+    unsigned long ms = millis();
+    float temporaryBpm = 20000. / (ms - bpmChronoStart);
+    bpmChronoStart = ms;
+    if ( temporaryBpm > 10 && temporaryBpm < 90 ) // make sure the BPM is within bounds
+      bpm = temporaryBpm;
+  }
+}
---
+++ Respiration.h
@@ -0,0 +1,125 @@
+ /*
+ * Respiration.h
+ *
+ * This class defines an object that can be used to gather information about
+ * respiration. There are many ways to gather respiration data, such as through thermistor,
+ * thermopile, conductive rubber cord, or piezoelectric signals.
+ *
+ * This file is part of the BioData project
+ * (c) 2018 Erin Gee
+ *
+ * Contributing authors:
+ * (c) 2018 Erin Gee
+ * (c) 2018 Sofian Audry
+ * (c) 2017 Thomas Ouellet Fredericks
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Average.h"
+#include "MinMax.h"
+#include "Threshold.h"
+#include "Lop.h"
+
+#ifndef RESP_H_
+#define RESP_H_
+
+class Respiration {
+
+  // Analog pin the Respiration sensor is connected to.
+  uint8_t _pin;
+
+  unsigned long bpmChronoStart;
+
+  MinMax respMinMax;
+  Threshold respThresh;
+
+  Lop respSensorAmplitudeLop;
+  Lop respSensorBpmLop;
+
+  float respSensorAmplitudeLopValue;
+
+  float respSensorBpmLopValue;
+  MinMax respSensorAmplitudeLopValueMinMax;
+
+  float respSensorAmplitudeLopValueMinMaxValue;
+  MinMax respSensorBpmLopValueMinMax;
+
+  float respSensorBpmLopValueMinMaxValue;
+
+  float respSensorFiltered;
+  float respSensorAmplitude;
+
+  float respSensorReading;
+
+  float bpm;  // this value is fed to initialize your BPM before a heartbeat is detected
+
+  bool breath;
+
+  // Sample rate in Hz.
+  unsigned long sampleRate;
+
+  // Internal use.
+  unsigned long microsBetweenSamples;
+  unsigned long prevSampleMicros;
+
+public:
+  Respiration(uint8_t pin, unsigned long rate=10);
+  virtual ~Respiration() {}
+
+  /// Resets all values.
+  void reset();
+
+  /// Sets sample rate.
+  void setSampleRate(unsigned long rate);
+
+  /**
+   * Reads the signal and perform filtering operations. Call this before
+   * calling any of the access functions. This function takes into account
+   * the sample rate.
+   */
+  void update();
+
+  /// Get normalized heartrate signal.
+  float getNormalized() const;
+
+  /// Returns true if a beat was detected during the last call to update().
+  bool breathDetected() const;
+
+  /// Returns BPM (beats per minute).
+  float getBPM() const;
+
+  /// Returns raw signal as returned by analogRead().
+  int getRaw() const;
+
+  ///Returns the average amplitude of signal mapped between 0.0 and 1.0.
+  /* For example, if amplitude is average, returns 0.5,
+   * if amplitude is below average, returns < 0.5
+   * if amplitude is above average, returns > 0.5.
+  */
+  float amplitudeChange() const;
+
+  /// Returns the average bpm of signal mapped between 0.0 and 1.0.
+  /* For example, if bpm is average, returns 0.5,
+    * if bpm is below average, returns < 0.5
+    * if bpm is above average, returns > 0.5.
+  */
+  float bpmChange() const;
+
+  // Performs the actual adjustments of signals and filterings.
+  // Internal use: don't use directly, use update() instead.
+  void sample();
+};
+
+#endif
