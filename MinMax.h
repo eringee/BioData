56f89564f2e8cee4cffedb6751c92d0347ceb0e5
Sofian Audry <info@sofianaudry.com>
2018-05-01 18:40:01 -0400

    Basic cleanup in MinMax code.

--- MinMax.h
+++ MinMax.h
@@ -1,27 +1,27 @@
 /******************************************************
- * This file is part of the BioData project
- * (c) 2018 Erin Gee   http://www.eringee.net
- *
- * This file normalizes values using their minimum and maximum values.
- *
- * Contributing authors:
- * (c) 2018 Erin Gee
- * (c) 2018 Sofian Audry
- * (c) 2017 Thomas Ouellet Fredericks
- * (c) 2017 Martin Peach
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   This file is part of the BioData project
+   (c) 2018 Erin Gee   http://www.eringee.net
+
+   This file normalizes values using their minimum and maximum values.
+
+   Contributing authors:
+   (c) 2018 Erin Gee
+   (c) 2018 Sofian Audry
+   (c) 2017 Thomas Ouellet Fredericks
+   (c) 2017 Martin Peach
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ******************************************************/
 
@@ -29,7 +29,7 @@
 #define MIN_MAX_H_
 
 class MinMax {
-    float input= 0;
+    float input = 0;
     float min = 0;
     float max = 0;
     float value = 0;
@@ -40,13 +40,12 @@ class MinMax {
 
     }
 
-
     void adapt(float lop) {
       lop = constrain(lop, 0, 1);
       lop = lop * lop;
 
-      min = (input - min) * lop + min;
-      max = (input - max) * lop + max;
+      min += (input - min) * lop;
+      max += (input - max) * lop;
     }
 
     float filter(float f) {
@@ -62,25 +61,20 @@ class MinMax {
         if ( f < min ) min = f;
       }
 
-
       if ( max == min ) {
         value = 0.5;
       } else {
         value = (f - min) / ( max - min);
-
       }
 
       return value;
     }
 
     float getMax() {
-
       return max;
     }
 
-
     float getMin() {
-
       return min;
     }
 
