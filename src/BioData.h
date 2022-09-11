81bbc554be4e98bf22d2c3b82b765d587cb1c697
Sofian Audry <info@sofianaudry.com>
2022-09-10 21:59:32 -0400

    Added core file BioData.h.

---
+++ src/BioData.h
@@ -0,0 +1,36 @@
+/*
+ * Heart.h
+ *
+ * This class defines an object that can be used to gather information about
+ * pulse as gathered through a photoplethysmograph such as the Pulse Sensor.
+ *
+ * This file is part of the BioData project
+ * (c) 2018 Erin Gee
+ *
+ * Contributing authors:
+ * (c) 2018 Erin Gee
+ * (c) 2017 Thomas Ouellet Fredericks
+ * (c) 2018 Sofian Audry
+ * (c) 2017 Martin Peach
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef BIO_DATA_H_
+#define BIO_DATA_H_
+
+#include "Heart.h"
+#include "SkinConductance.h"
+#include "Respiration.h"
+
+#endif
