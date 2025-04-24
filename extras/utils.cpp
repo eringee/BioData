62eabf360713aaf8a8655c82008286c4a2be3b96
possibly-human <luana_belinsky@hotmail.com>
2025-04-24 13:36:37 -0400

    utils reset

--- extras/utils.cpp
+++ extras/utils.cpp
@@ -1,18 +1,18 @@
 // utils.cpp
 #include "utils.h"
 
-inline float clamp(float value, float min, float max) {
+float clamp(float value, float min, float max) {
     return (value < min) ? min : (value > max) ? max : value;
 }
 
-inline float map(float value, float min1, float max1, float min2, float max2) {
+float map(float value, float min1, float max1, float min2, float max2) {
     return (value - min1) / (max1 - min1) * (max2 - min2) + min2;
 }
 
-inline float min(float a, float b) {
+float min(float a, float b) {
     return (a < b) ? a : b; 
 }
 
-inline float max(float a, float b){
+float max(float a, float b){
     return (a > b) ? a : b;
 }
